978385ba091170ff51b9b0312b1db662b5c0b8ec
emersonhenriquedev <emerson.hm@outlook.com>
2023-10-23 18:31:05 -0300

    add flex layout

--- src/app/shared/material/users-mat.module.ts
+++ src/app/shared/material/users-mat.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   exports: [
     MatTabsModule,
     MatIconModule,
+    MatButtonModule,
   ],
   declarations: [],
   providers: []
